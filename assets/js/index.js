295c4acb6c4c238788736a129714e1695c118b10
Andrey212 <tarangad313@gmail.com>
2019-12-09 17:41:11 +0200

    Firsr commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -32,19 +32,42 @@ new Promise(loadUser)
 
 fetch('./user.json')
     .then(response => response.json())
-    .then( appendUsersToList )
+    .then(appendUsersToList)
     .catch(console.error);
 
 
-
-
 function appendUsersToList(users) {
+    const usersList = document.getElementById('usersList');
+    users.forEach((user) => {
+        const usersListItem = document.createElement('li');
+        usersListItem.classList.add('userListItem');
+        usersListItem.setAttribute('id', `${user.id}`);
+        usersListItem.appendChild(createUserCardContainer(user));
+        usersListItem.appendChild(addUserName(user));
+        usersList.append(usersListItem);
+    });
 
 }
 
+function createUserCardContainer(user) {
+    const userCardWrapper = document.createElement('div');
+    userCardWrapper.classList.add('profilePictureContainer');
+    userCardWrapper.appendChild(createUserImage(user));
+    return userCardWrapper;
+}
 
+function createUserImage(user) {
+    const addUserPicture = document.createElement('img');
+    addUserPicture.setAttribute('src', user.profilePictureSrc);
+    addUserPicture.setAttribute('alt', 'user picture');
+    return addUserPicture;
+}
 
-
+function addUserName(user) {
+    const userName = document.createElement('h3');
+    userName.innerText = `${user.name} ${user.surname}`;
+    return userName;
+}
 
 
 
